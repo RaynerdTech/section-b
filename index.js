654d332a20a9cfb4c99989eeb48612722bd8a7a5
RaynerdTech <144237574+RaynerdTech@users.noreply.github.com>
2024-07-29 20:00:49 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,18 @@
+const myStudent = ["Abdulrahmon", "Emmanuel", "Amarachi", "Franklin", "Austin", "Joseph", "Abdulwahad", "Naomi", "Josh", "Eniola"]
+
+for (let i = 0; i < myStudent.length; i++) {
+    console.log(myStudent[i]);
+}
+
+
+const person = {
+    name: 'John Doe',      // String
+    age: 30,               // Number
+    married: false,        // Boolean
+    location: undefined    // Undefined
+};
+
+console.log('Name:', person.name);
+console.log('Age:', person.age);
+console.log('Married:', person.married);
+console.log('Location:', person.location);
